544fd4b33747bded6d35fffa86aa44786f54e653
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-05-27 22:26:42 +0200

    Finish query 4
    Signed-off-by: Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>

--- solutions.sql
+++ solutions.sql
@@ -1,18 +1,24 @@
--- Query 1 : Get the id values of the first 5 clients from district_id with a value equals to 1.
+-- Query 1: Get the id values of the first 5 clients from district_id with a value equals to 1.
 SELECT client_id
 FROM client
 WHERE district_id = 1
 LIMIT 5;
 
--- Query 2 : In the client table, get an id value of the last client where the district_id equals to 72.
+-- Query 2: In the client table, get an id value of the last client where the district_id equals to 72.
 SELECT client_id
 FROM client
 WHERE district_id = 72
 ORDER BY client_id DESC
 limit 1;
 
--- Query 3 : Get the 3 lowest amounts in the loan table.
+-- Query 3: Get the 3 lowest amounts in the loan table.
 SELECT amount
 FROM loan
 ORDER BY amount
 limit 3;
+
+-- Query 4: What are the possible values for status, ordered alphabetically in ascending order in the loan table?
+SELECT status
+FROM loan
+GROUP BY status
+ORDER BY status
