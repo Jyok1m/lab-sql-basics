abcf23e4a0cd42a02cf3e9f68c5aed6f404e9b28
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-05-27 22:53:42 +0200

    Done query 14
    Signed-off-by: Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>

--- solutions.sql
+++ solutions.sql
@@ -69,9 +69,15 @@ WHERE account_id = 793
 ORDER BY date DESC
 LIMIT 10;
 
---Query 13: In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
+-- Query 13: In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
 SELECT district_id, count(client_id)
 FROM client
 WHERE district_id < 10
 GROUP BY district_id
-ORDER BY district_id
+ORDER BY district_id;
+
+-- Query 14: In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
+SELECT type, count(type)
+FROM card
+GROUP BY type
+ORDER BY count(type) DESC;
