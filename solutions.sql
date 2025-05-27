b24a7671ae0df177f64296d1412352847b715633
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-05-27 22:51:47 +0200

    Done query 13
    Signed-off-by: Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>

--- solutions.sql
+++ solutions.sql
@@ -68,3 +68,10 @@ FROM trans
 WHERE account_id = 793
 ORDER BY date DESC
 LIMIT 10;
+
+--Query 13: In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
+SELECT district_id, count(client_id)
+FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id
